c91b8089ce53d0c3904d271c427fbd624a8410d4
andy.rothwell <rothwell.andy@gmail.com>
2019-01-02 14:43:02 -0500

    testing library build

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import TopicComponent from './TopicComponent.vue';
+import ExternalLink from './ExternalLink.vue';
+import Badge from './Badge.vue';
+
+
+const Components = {
+  TopicComponent,
+  ExternalLink,
+  Badge
+}
+
+Object.keys(Components).forEach(name => {
+  Vue.component(name, Components[name]);
+})
+
+export default Components;
+
+// export { TopicComponent };
+// export { ExternalLink };
+// export { Badge };
