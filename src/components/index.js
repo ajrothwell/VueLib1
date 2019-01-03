4070ee272e667a6f4b767a36f6ec255c0b744b8c
andy.rothwell <rothwell.andy@gmail.com>
2019-01-03 10:04:22 -0500

    does not run vue in lib

--- src/components/index.js
+++ src/components/index.js
@@ -12,9 +12,9 @@ const Components = {
   Test
 }
 
-Object.keys(Components).forEach(name => {
-  Vue.component(name, Components[name]);
-})
+// Object.keys(Components).forEach(name => {
+//   Vue.component(name, Components[name]);
+// })
 
 export default Components;
 
